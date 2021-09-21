8fd262e1a3041c66ed8ca1ee55c012af493fd5da
Missael Ortiz <ortiz.missael80@gmail.com>
2021-09-21 13:31:17 -0700

    added working state dependant renders

--- src/containers/ArtistSearch.jsx
+++ src/containers/ArtistSearch.jsx
@@ -6,7 +6,7 @@ import { getArtist } from '../services/musicApi';
 const ArtistSearch = () => {
   const [loading, setLoading] = useState(true);
   const [artists, setArtists] = useState([]);
-  const [search, setSearch] = useState('death');
+  const [search, setSearch] = useState('');
 
   useEffect(() => {
     getArtist(search)
@@ -15,8 +15,9 @@ const ArtistSearch = () => {
   }, []);
 
   useEffect(() => {
-    getArtist(search)
+    if(search) getArtist(search)
       .then((res) => setArtists(res));
+    if(!search) setArtists([]);
   }, [search]);
 
   const handleSearch = ({ target }) => {
@@ -24,8 +25,13 @@ const ArtistSearch = () => {
   };
 
   if(loading) return <h1>Loading...</h1>;
-
-  return (
+  if(search === '') return (
+    <>
+      <Search search={search} onSearch={handleSearch}/>
+      <h1>No Artists Found</h1>
+    </>
+  );
+  if(artists !== []) return (
     <>
       <Search search={search} onSearch={handleSearch}/>
       <ArtistList artists={artists} search={search} />
