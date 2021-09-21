876f4b9785fca71041b7f1876d55e18ec0b2a93b
Missael Ortiz <ortiz.missael80@gmail.com>
2021-09-21 14:30:36 -0700

    added routes to app.jsx

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,6 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import ArtistSearch from '../../containers/artistSearch';
 
 export default function App() {
-  return <ArtistSearch />;
+  return (
+    <Router>
+      <header>
+        <nav>
+          <Link to="/"> Home </Link>
+        </nav>
+      </header>
+      <Switch>
+        <Route exact path="/">
+          <ArtistSearch />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
--- src/components/artists/Artist.jsx
+++ src/components/artists/Artist.jsx
@@ -2,22 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Artist = ({ artist }) => {
-  const { name, type, id, country } = artist;
+  const { name, country } = artist;
   
   return (
     <>
       <h2>{name}</h2>
-      <p>{type}</p>
       <p>{country}</p>
-      <p>{id}</p>
     </>
   );
 };
 
 Artist.propTypes = {
   artist: PropTypes.shape({
-    id: PropTypes.string.isRequired,
-    type: PropTypes.string,
     name: PropTypes.string.isRequired,
     country: PropTypes.string,
   })
